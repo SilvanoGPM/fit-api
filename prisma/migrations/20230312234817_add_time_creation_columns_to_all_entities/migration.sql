517bc4b5cd252e06a4765dbf1a16616e1edac3af
SkyG0D <silvanosilvino@hotmail.com>
2023-03-12 20:49:41 -0300

    chore: add new migrations

---
+++ prisma/migrations/20230312234817_add_time_creation_columns_to_all_entities/migration.sql
@@ -0,0 +1,26 @@
+/*
+  Warnings:
+
+  - Added the required column `updatedAt` to the `Exercice` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Food` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `RefreshToken` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "Exercice" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Food" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "RefreshToken" ADD COLUMN     "browser" TEXT NOT NULL DEFAULT '',
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "os" TEXT NOT NULL DEFAULT '',
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "User" ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
---
+++ prisma/migrations/20230312234916_add_default_value_to_browser_and_os_in_refresh_token/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "RefreshToken" ALTER COLUMN "browser" SET DEFAULT 'Unknown',
+ALTER COLUMN "os" SET DEFAULT 'Unknown';
