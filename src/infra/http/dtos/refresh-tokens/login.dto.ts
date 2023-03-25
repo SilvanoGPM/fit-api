08b131391befe395fa76cbe5a70f494898fd83a7
SkyG0D <silvanosilvino@hotmail.com>
2023-03-24 21:07:23 -0300

    docs: add refresh tokens dtos docs

--- src/infra/http/dtos/refresh-tokens/login.dto.ts
+++ src/infra/http/dtos/refresh-tokens/login.dto.ts
@@ -1,10 +1,13 @@
 import { IsEmail, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class LoginDTO {
   @IsNotEmpty()
   @IsEmail()
+  @ApiProperty({ description: 'E-mail do usuário.' })
   email: string;
 
   @IsNotEmpty()
+  @ApiProperty({ description: 'Senha do usuário' })
   password: string;
 }
--- src/infra/http/dtos/refresh-tokens/refresh-token.dto.ts
+++ src/infra/http/dtos/refresh-tokens/refresh-token.dto.ts
@@ -1,7 +1,9 @@
 import { IsJWT, IsNotEmpty } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class RefreshTokenDTO {
   @IsNotEmpty()
   @IsJWT()
+  @ApiProperty({ description: 'Refresh token para realizar o login.' })
   token: string;
 }
